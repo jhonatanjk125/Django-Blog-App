1453aa4b84ddb555171e0243d2cc1fc54e304fdc
jhonatanjk125 <jhonatanjk125@gmail.com>
2023-10-22 05:33:20 -0500

    Collect static files

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'upload'
 LOGIN_REDIRECT_URL = '/'
